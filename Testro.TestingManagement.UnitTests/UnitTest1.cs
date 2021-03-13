5c47e5186c4839ad37a3e391a8c58eed0cdb4fc9
Mateusz Majewski <mateuszmajewski@Mateuszs-MacBook-Air.local>
2021-03-13 23:32:06 +0100

    Initial commit

---
+++ Testro.TestingManagement.UnitTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace Testro.TestingManagement.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
