b180e58852dc7b7acc3eddb7b4c2b400a43a47f7
Mateusz Majewski <mati.majewski@wp.pl>
2021-04-13 00:53:33 +0200

    added registration

--- Testro.TestingManagement.WebApi/Controllers/AuthManagementController.cs
+++ Testro.TestingManagement.WebApi/Controllers/AuthManagementController.cs
@@ -1,7 +1,15 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 using Testro.TestingManagement.WebApi.Configurations;
+using Testro.TestingManagement.WebApi.Dtos;
 
 namespace Testro.TestingManagement.WebApi.Controllers
 {
@@ -16,6 +24,52 @@ namespace Testro.TestingManagement.WebApi.Controllers
         {
             _userManager = userManager;
             _jwtConfig = optionsMonitor.CurrentValue;
+        }
+
+        [HttpPost("Register")]
+        public async Task<IActionResult> Register([FromBody] UserRegistrationDto user)
+        {
+            var existingUser = await _userManager.FindByEmailAsync(user.Email);
+            if (existingUser is not null)
+            {
+                return BadRequest("User already exists");
+            }
+
+            var newUser = new IdentityUser() {Email = user.Email, UserName = user.Email,};
+            var isCreated = await _userManager.CreateAsync(newUser, user.Password);
+            if (isCreated.Succeeded)
+            {
+                var jwtToken = GenerateJwtToken(newUser);
+
+                return Ok(jwtToken);
+            }
+
+            return BadRequest(isCreated.Errors.Select(x => x.Description).ToList());
+        }
+
+        private string GenerateJwtToken(IdentityUser user)
+        {
+            var jwtTokenHandler = new JwtSecurityTokenHandler();
+
+            var key = Encoding.ASCII.GetBytes(_jwtConfig.Secret);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+                    new Claim("id", user.Id),
+                    new Claim(JwtRegisteredClaimNames.Email, user.Email),
+                    new Claim(JwtRegisteredClaimNames.Sub, user.Email),
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+                }),
+                Expires = DateTime.UtcNow.AddHours(6),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = jwtTokenHandler.CreateToken(tokenDescriptor);
+            var jwtToken = jwtTokenHandler.WriteToken(token);
+
+            return jwtToken;
         }
     }
 }
