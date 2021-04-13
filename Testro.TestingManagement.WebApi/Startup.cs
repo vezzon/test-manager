82c3c11a7f26757393310673c640c5371d00fb1f
Mateusz Majewski <mati.majewski@wp.pl>
2021-04-13 22:09:06 +0200

    added authorization to swagger

--- Testro.TestingManagement.WebApi/Startup.cs
+++ Testro.TestingManagement.WebApi/Startup.cs
@@ -107,6 +107,29 @@ namespace Testro.TestingManagement.WebApi
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo {Title = "Testro.TestingManagement.WebApi", Version = "v1"});
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "JWT Authorization header using the Bearer scheme."
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference 
+                            { 
+                                Type = ReferenceType.SecurityScheme, 
+                                Id = "Bearer" 
+                            }
+                        },
+                        Array.Empty<string>()
+                    }
+                });
             });
         }
 
